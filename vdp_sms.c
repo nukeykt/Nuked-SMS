9e95ea5f5af1642b0f01c2e095bb5a5a917f609f
nukeykt <alexeytf2@gmail.com>
2023-09-03 19:03:32 +0900

    ym2602 skeleton

---
+++ vdp_sms.c
@@ -0,0 +1,7 @@
+// YM2602 core
+
+#include "vdp_sms.h"
+
+void VDPSMS_Clock(vdpsms_t *chip, int clk)
+{
+}
---
+++ vdp_sms.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef struct {
+    int tm_w1;
+    int tm_w2;
+    int tm_w3;
+    int tm_w4;
+    int tm_w5;
+    int tm_w6;
+    int tm_w7;
+    int tm_w8;
+    int tm_w9;
+} vdpsms_t;

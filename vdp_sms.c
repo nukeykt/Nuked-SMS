ce44b084ee358ee68dc34517f66fe8c14789494f
nukeykt <alexeytf2@gmail.com>
2023-09-08 18:36:44 +0900

    ym2602: vram io pads

--- vdp_sms.c
+++ vdp_sms.c
@@ -4,4 +4,51 @@
 
 void VDPSMS_Clock(vdpsms_t *chip, int clk)
 {
+    if (chip->clk2)
+        chip->w1 = chip->input.ad_i;
+    if (chip->clk1)
+    {
+        chip->w2_h = (chip->w1 >> 8) & 255;
+        chip->w2_l = (chip->w1 >> 0) & 255;
+    }
+    if (chip->tm_w1)
+        chip->vram_data = w2_h;
+    if (chip->tm_w2)
+        chip->vram_data = w2_l;
+
+    if (chip->tm_w1)
+        chip->w3 = (chip->input.ad_i >> 8) & 255;
+    if (chip->tm_w2)
+    {
+        chip->vram_address &= ~0x1fc0;
+        chip->vram_address |= (w3 & 254) << 5;
+    }
+    if (chip->tm_w3)
+    {
+        chip->vram_address &= ~0x20;
+        chip->vram_address |= (w3 & 1) << 5;
+    }
+
+    if (chip->clk2)
+        chip->w4 = (chip->vram_data & 255) | ((chip->vram_data & 255) << 8);
+    if (chip->tm_w1)
+        chip->w5 = chip->w4;
+
+    if (chip->tm_w1)
+        chip->w6 = (chip->vram_address >> 1) & 0x1fff;
+
+    if (chip->tm_w1)
+        chip->w7 = chip->w5;
+    if (chip->tm_w2)
+        chip->w7 = chip->w6;
+
+    if (chip->tm_w1)
+    {
+        chip->ad_o_z = 1;
+    }
+    else
+    {
+        chip->ad_o_z = 0;
+        chip->ad_o = chip->w7;
+    }
 }
--- vdp_sms.h
+++ vdp_sms.h
@@ -1,6 +1,31 @@
 #pragma once
 
 typedef struct {
+    int ad_i;
+
+} vdpsms_input_t;
+
+typedef struct {
+    vdpsms_input_t input;
+
+    int ad_o;
+    int ad_o_z;
+
+    int w1;
+    int w2_h;
+    int w2_l;
+    int w3;
+    int w4;
+    int w5;
+    int w6;
+    int w7;
+
+    int vram_data;
+    int vram_address;
+
+    int clk1;
+    int clk2;
+
     int tm_w1;
     int tm_w2;
     int tm_w3;
